460c84658f022f8c7057db053f78bbb0f303855b
Lukasz <czarnymwkp@example.com>
2026-02-19 09:36:20 +0100

    I add red star to inputs in register form (Showing only when is required)

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Flex, TextField } from '@radix-ui/themes';
+import { Box, Flex, Text, TextField } from '@radix-ui/themes';
 import { AlertCircle } from 'lucide-react';
 import Link from 'next/link';
 import { useForm } from 'react-hook-form';
@@ -38,9 +38,35 @@ export default function OwnerRegister() {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = form;
   const { error, handleRegister } = useRegister();
-
+  const validationSchema = {
+    name: {
+      required: t('register_page.name_required'),
+      minLength: { value: 10, message: t('register_page.name_too_short') },
+    },
+    companyName: {
+      required: t('register_page.company_required'),
+      minLength: { value: 2, message: t('register_page.company_too_short') },
+    },
+    email: {
+      required: t('register_page.email_required'),
+      minLength: { value: 5, message: t('register_page.email_too_short') },
+    },
+    password: {
+      required: t('register_page.password_required'),
+      minLength: { value: 6, message: t('register_page.password_too_short') },
+    },
+    confirmPassword: {
+      required: t('register_page.confirm_password_required'),
+      validate: (val: string) => {
+        if (watch('password') != val) {
+          return t('register_page.passwords_must_match');
+        }
+      },
+    },
+  };
   return (
     <RegisterWrapper>
       <h1 className=" text-center text-3xl font-bold text-foreground mb-2">
@@ -51,90 +77,79 @@ export default function OwnerRegister() {
         <form onSubmit={handleSubmit(handleRegister)}>
           <Flex direction="column" gap="2">
             <Box>
-              <div className="mb-2">
-                <label className="text-sm font-medium text-foreground block">
-                  {t('register_page.name_label')}
-                </label>
-              </div>
+              <Text as="label" size="2" weight="medium">
+                {t('register_page.name_label')}
+                {validationSchema.name.required ? <span className="text-red-500">*</span> : null}
+              </Text>
               <TextField.Root
                 size="3"
                 variant="surface"
                 type="text"
                 placeholder={t('register_page.name_placeholder')}
-                {...register('name', {
-                  required: t('register_page.name_required'),
-                  minLength: { value: 10, message: t('register_page.name_too_short') },
-                })}
+                {...register('name', validationSchema.name)}
               />
               <ErrorLabel error={errors.name?.message} id="name" />
             </Box>
             <Box>
-              <div className="mb-2">
-                <label className="text-sm font-medium text-foreground block">
-                  {t('register_page.company_label')}
-                </label>
-              </div>
+              <Text as="label" size="2" weight="medium">
+                {t('register_page.company_label')}
+                {validationSchema.companyName.required ? (
+                  <span className="text-red-500">*</span>
+                ) : null}
+              </Text>
               <TextField.Root
                 size="3"
                 variant="surface"
                 type="text"
                 placeholder={t('register_page.company_placeholder')}
-                {...register('companyName', {
-                  required: t('register_page.company_required'),
-                  minLength: { value: 2, message: t('register_page.company_too_short') },
-                })}
+                {...register('companyName', validationSchema.companyName)}
               />
               <ErrorLabel error={errors.companyName?.message} id="companyName" />
             </Box>
             <Box>
-              <div className="mb-2">
-                <label className="text-sm font-medium text-foreground block">
-                  {t('register_page.email_label')}
-                </label>
-              </div>
+              <Text as="label" size="2" weight="medium">
+                {t('register_page.email_label')}
+                {validationSchema.email.required ? <span className="text-red-500">*</span> : null}
+              </Text>
               <TextField.Root
                 size="3"
                 variant="surface"
                 type="email"
                 placeholder={t('register_page.email_placeholder')}
-                {...register('email', {
-                  required: t('register_page.email_required'),
-                  minLength: { value: 5, message: t('register_page.email_too_short') },
-                })}
+                {...register('email', validationSchema.email)}
               />
               <ErrorLabel error={errors.email?.message} id="email" />
             </Box>
             <Box>
-              <div className="mb-2">
-                <label className="text-sm font-medium text-foreground block">
-                  {t('register_page.password_label')}
-                </label>
-              </div>
+              <Text as="label" size="2" weight="medium">
+                {t('register_page.password_label')}
+                {validationSchema.password.required ? (
+                  <span className="text-red-500">*</span>
+                ) : null}
+              </Text>
               <TextField.Root
                 size="3"
                 variant="surface"
                 type="password"
                 placeholder={t('register_page.password_placeholder')}
-                {...register('password', {
-                  required: t('register_page.password_required'),
-                  minLength: { value: 6, message: t('register_page.password_too_short') },
-                })}
+                {...register('password', validationSchema.password)}
               />
               <ErrorLabel error={errors.password?.message} id="password" />
             </Box>
             <Box>
-              <div className="mb-2">
-                <label className="text-sm font-medium text-foreground block">
-                  {t('register_page.confirm_password_label')}
-                </label>
-              </div>
+              <Text as="label" size="2" weight="medium">
+                {t('register_page.confirm_password_label')}
+                {validationSchema.confirmPassword.required ? (
+                  <span className="text-red-500">*</span>
+                ) : null}
+              </Text>
               <TextField.Root
                 size="3"
                 variant="surface"
                 type="password"
                 placeholder={t('register_page.confirm_password_placeholder')}
                 {...register('confirmPassword', {
-                  required: t('register_page.confirm_password_required'),
+                  ...validationSchema.confirmPassword,
                   validate: (val: string) => {
                     if (watch('password') != val) {
                       return t('register_page.passwords_must_match');
