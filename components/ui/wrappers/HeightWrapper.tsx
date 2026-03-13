eacb2c7a44f9e9a181f9c02179bb59fb286db2fe
Lukasz <czarnymwkp@example.com>
2026-03-13 10:21:34 +0100

    Removed I18Next provider, fix wrapper to make one height for site.

---
+++ components/ui/wrappers/HeightWrapper.tsx
@@ -0,0 +1,5 @@
+const HeightWrapper = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex-1 flex flex-col w-full">{children}</div>;
+};
+
+export default HeightWrapper;
